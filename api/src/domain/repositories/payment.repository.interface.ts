72ac4290267be7b6e7f75ec3da17b2362be37831
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-09-24 23:42:26 -0300

    ajustes finais

--- api/src/domain/repositories/payment.repository.interface.ts
+++ api/src/domain/repositories/payment.repository.interface.ts
@@ -1,11 +1,21 @@
 import { Payment } from '../entities/payment.entity';
+import { PaymentMethod } from '../enums/payment-method.enum';
 import { PaymentStatus } from '../enums/payment-status.enum';
 
 export interface IPaymentRepository {
-  create(data: Partial<Payment>): Promise<Payment>;
+   create(data: {
+    id?: string;
+    cpf: string;
+    description: string;
+    amount: number;
+    paymentMethod: PaymentMethod;
+    status: PaymentStatus;
+    externalId?: string | null;
+  }): Promise<Payment>;
+
   update(id: string, data: Partial<Payment>): Promise<Payment>;
   findById(id: string): Promise<Payment | null>;
-  findAll(filters?: { cpf?: string; paymentMethod?: string; status?: PaymentStatus }): Promise<Payment[]>;
+  findAll(filters?: { cpf?: string; paymentMethod?: PaymentMethod; status?: PaymentStatus }): Promise<Payment[]>;
   updateStatus(id: string, status: PaymentStatus): Promise<Payment>;
   setExternalId(id: string, externalId: string): Promise<Payment>;
 }
--- api/src/main.ts
+++ api/src/main.ts
@@ -2,25 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Payments API')
-    .setDescription('API para gerenciamento de pagamentos (PIX e Cartão)')
+    .setDescription('API para gerenciar pagamentos (PIX e Cartão)')
     .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT || 3000;
+  const port = Number(process.env.PORT || 3000);
   await app.listen(port);
-  console.log(`🚀 Payments API is running on http://localhost:${port}`);
+  console.log(`Listening on http://localhost:${port}/api`);
 }
-
 bootstrap();
